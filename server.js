3f1e1328e27650ac7a683558fe8304b1248f42db
Dave <drregg6@gmail.com>
2019-04-15 12:38:40 -0400

    server code is up and running - connected to mongodb

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// bodyParser middleware
+app.use(bodyParser.json());
+
+// database
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const port = process.env.PORT || 5000;
+
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('connected'))
+    .catch(err => console.log(err));
+
+app.listen(port, () => console.log(`listening on ${port}`));
