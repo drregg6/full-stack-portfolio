ca6e47283d9e1d9ac786710188e180dea7c44c5e
Dave <drregg6@gmail.com>
2019-04-18 16:20:33 -0400

    adjusting portfolio css - overriding reactstrap styles

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,10 +1,85 @@
 import React, { Component } from 'react';
+import {
+    Container,
+    CardDeck,
+    Card,
+    CardImg,
+    CardImgOverlay,
+    CardText,
+    CardBody,
+    CardTitle,
+    CardSubtitle,
+    Button
+} from 'reactstrap';
 
 class Portfolio extends Component {
     render() {
         return (
             <div className="custom-portfolio" id="portfolio">
-                <h1>PORTFOLIO</h1>
+                <CardDeck>
+                    <Card>
+                        <CardImg top width="100%" src="https://placehold.it/350x350" alt="Placehold it!" />
+                        <CardImgOverlay>
+                            <div className="portfolio-title">
+                                <CardTitle>Card Title</CardTitle>
+                                <CardText>Hello world</CardText>
+                            </div>
+                            <Button outline color="primary">See More</Button>
+                        </CardImgOverlay>
+                    </Card>
+                    <Card>
+                        <CardImg top width="100%" src="https://placehold.it/350x350" alt="Placehold it!" />
+                        <CardImgOverlay>
+                            <div className="portfolio-title">
+                                <CardTitle>Card Title</CardTitle>
+                                <CardText>Hello world</CardText>
+                            </div>
+                            <Button outline color="primary">See More</Button>
+                        </CardImgOverlay>
+                    </Card>
+                    <Card>
+                        <CardImg top width="100%" src="https://placehold.it/350x350" alt="Placehold it!" />
+                        <CardImgOverlay>
+                            <div className="portfolio-title">
+                                <CardTitle>Card Title</CardTitle>
+                                <CardText>Hello world</CardText>
+                            </div>
+                            <Button outline color="primary">See More</Button>
+                        </CardImgOverlay>
+                    </Card>
+                </CardDeck>
+                <CardDeck>
+                    <Card>
+                        <CardImg top width="100%" src="https://placehold.it/350x350" alt="Placehold it!" />
+                        <CardImgOverlay>
+                            <div className="portfolio-title">
+                                <CardTitle>Card Title</CardTitle>
+                                <CardText>Hello world</CardText>
+                            </div>
+                            <Button outline color="primary">See More</Button>
+                        </CardImgOverlay>
+                    </Card>
+                    <Card>
+                        <CardImg top width="100%" src="https://placehold.it/350x350" alt="Placehold it!" />
+                        <CardImgOverlay>
+                            <div className="portfolio-title">
+                                <CardTitle>Card Title</CardTitle>
+                                <CardText>Hello world</CardText>
+                            </div>
+                            <Button outline color="primary">See More</Button>
+                        </CardImgOverlay>
+                    </Card>
+                    <Card>
+                        <CardImg top width="100%" src="https://placehold.it/350x350" alt="Placehold it!" />
+                        <CardImgOverlay>
+                            <div className="portfolio-title">
+                                <CardTitle>Card Title</CardTitle>
+                                <CardText>Hello world</CardText>
+                            </div>
+                            <Button outline color="primary">See More</Button>
+                        </CardImgOverlay>
+                    </Card>
+                </CardDeck>
             </div>
         )
     }
